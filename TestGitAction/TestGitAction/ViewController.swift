1ad4e819dbe07e6a28aa31bc51749f38c8af1c2f
longvt-1468 <vu.thanh.long@sun-asterisk.com>
2019-09-10 14:50:31 +0700

    add_project

---
+++ TestGitAction/TestGitAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitAction
+//
+//  Created by vu.thanh.long on 9/10/19.
+//  Copyright © 2019 vu.thanh.long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
